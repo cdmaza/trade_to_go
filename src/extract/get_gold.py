3eb29fba7482b26e1479f4ebe1276007f115046e
cdmaza <muhammadakhmalraziq@gmail.com>
2026-05-28 12:43:32 +0800

    extract gold file

---
+++ src/extract/get_gold.py
@@ -0,0 +1,29 @@
+import websocket
+import json
+
+result = {}
+
+def on_message(ws, message):
+    data = json.loads(message)
+    result.update(data)
+    ws.close()
+
+def on_error(ws, error):
+    print("Error:", error)
+
+headers = {
+    "Origin": "https://bgd.bursamalaysia.com",
+    "Host": "bgd-adam.bursamalaysia.com",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36",
+}
+
+app = websocket.WebSocketApp(
+    "wss://bgd-adam.bursamalaysia.com/bgd/pricestream",
+    on_message=on_message,
+    on_error=on_error,
+    header=headers,
+)
+app.run_forever()
+
+print("Sell Price:", result.get("bursaBuyPrice"))
+print("Buy Price:", result.get("bursaSellPrice"))
